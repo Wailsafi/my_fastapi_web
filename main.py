0b4fd10099b77f21ef2eee0144c92a9a85b6bd0d
Wailsafi <safiwail2001@gmail.com>
2026-01-28 19:26:25 +0100

    adding static files  and updating the templates

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+
+
 
 from fastapi.templating import Jinja2Templates
 
@@ -8,14 +11,14 @@ from fastapi.templating import Jinja2Templates
 posts: list[dict] = [
     {
         "id": 1,
-        "author": "Corey Schafer",
+        "author": "Wail",
         "title": "FastAPI is Awesome",
         "content": "This framework is really easy to use and super fast.",
         "date_posted": "April 20, 2025",
     },
     {
         "id": 2,
-        "author": "Jane Doe",
+        "author": "wassim",
         "title": "Python is Great for Web Development",
         "content": "Python is a great language for web development, and FastAPI makes it even better.",
         "date_posted": "April 21, 2025",
@@ -24,6 +27,7 @@ posts: list[dict] = [
 
 app=FastAPI()
 templates=Jinja2Templates(directory="templates")
+app.mount("/static",StaticFiles(directory="static"),name="static")
 
 
 @app.get("/", include_in_schema=False)   # include_in_schema : to allow the router works but it does not apear in the docs of fastapi 
@@ -34,6 +38,9 @@ def home(request: Request):
 @app.get("/api/posts")
 def get_posts():
     return  posts 
+
+
+
 
 
 
